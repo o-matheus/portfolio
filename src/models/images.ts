f112158e12022f34e876ce82d99e98684c31869b
Matheus <matheusaguiar028@gmail.com>
2025-12-16 09:37:11 -0300

    fix: Correct casing in import path for educaAI image

--- src/models/images.ts
+++ src/models/images.ts
@@ -10,7 +10,7 @@ import imc from '../assets/imgs/projetos/IMC.png'
 import imcDesktop from '../assets/imgs/gif/imcDesktop.gif'
 import lTarefas from '../assets/imgs/projetos/lTarefas.png'
 import orv from '../assets/imgs/projetos/ORV.png'
-import educaAI from '../assets/imgs/projetos/educaAi.png'
+import educaAI from '../assets/imgs/projetos/educaAI.png'
 import mabi from '../assets/imgs/projetos/mabi.png'
 
 
