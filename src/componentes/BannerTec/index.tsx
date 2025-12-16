422bdff843380f8413f941852b2d952f157142d3
Matheus <matheusaguiar028@gmail.com>
2025-12-16 10:02:49 -0300

    fix: Update import statements for tech icons in BannerTec component

--- src/componentes/BannerTec/index.tsx
+++ src/componentes/BannerTec/index.tsx
@@ -1,20 +1,31 @@
 import bStrap from "../../assets/imgs/stack/bStrap.svg"
+import css from "../../assets/imgs/stack/css.svg"
+import cypress from "../../assets/imgs/stack/cy.svg"
+import github from "../../assets/imgs/stack/github.svg"
+import html from "../../assets/imgs/stack/html.svg"
+import java from "../../assets/imgs/stack/java.svg"
+import javascript from "../../assets/imgs/stack/js.svg"
+import react from "../../assets/imgs/stack/react.svg"
+import sass from "../../assets/imgs/stack/sass.svg"
+import styled from "../../assets/imgs/stack/styledC.svg"
+import typescript from "../../assets/imgs/stack/ts.svg"
+import vue from "../../assets/imgs/stack/vue.svg"
 
 import { BannerWrapper, Slider, TechIcon, TechLabel, Track, TrackItem } from './styles'
 
 export const items = [
     {src: bStrap, name: "Bootstrap"},
-    {src: 'src/assets/imgs/stack/css.svg', name: "CSS"},
-    {src: 'src/assets/imgs/stack/cy.svg', name: "Cypress"},
-    {src: 'src/assets/imgs/stack/github.svg', name: "GitHub"},
-    {src: 'src/assets/imgs/stack/html.svg', name: "HTML"},
-    {src: 'src/assets/imgs/stack/java.svg', name: "Java"},
-    {src: 'src/assets/imgs/stack/js.svg', name: "JavaScript"},
-    {src: 'src/assets/imgs/stack/react.svg', name: "React"},
-    {src: 'src/assets/imgs/stack/sass.svg', name: "SASS"},
-    {src: 'src/assets/imgs/stack/styledC.svg', name: "StyledComponents"},
-    {src: 'src/assets/imgs/stack/ts.svg', name: "TypeScript"},
-    {src: 'src/assets/imgs/stack/vue.svg', name: "Vue.js"},
+    {src: css, name: "CSS"},
+    {src: cypress, name: "Cypress"},
+    {src: github, name: "GitHub"},
+    {src: html, name: "HTML"},
+    {src: java, name: "Java"},
+    {src: javascript, name: "JavaScript"},
+    {src: react, name: "React"},
+    {src: sass, name: "SASS"},
+    {src: styled, name: "StyledComponents"},
+    {src: typescript, name: "TypeScript"},
+    {src: vue, name: "Vue.js"},
 ]
 
 const BannerTec: React.FC = () => {
