3c2b7187f51ef00ce8514e89371f4074b9ec018f
Matheus <matheusaguiar028@gmail.com>
2025-12-16 11:08:28 -0300

    fix: Update video file extensions for project GIFs in images model

--- src/models/images.ts
+++ src/models/images.ts
@@ -1,9 +1,9 @@
 import imcDesktop from '../assets/imgs/gif/imcDesktop.mp4'
-import lContatosDesktop from '../assets/imgs/gif/lContatosDesktop.gif'
-import lTarefasDesktop from '../assets/imgs/gif/lTarefasDesktop.gif'
-import orvDesktop from '../assets/imgs/gif/orvDesktop.gif'
-import educaDesktop from '../assets/imgs/gif/educaDesktop.webp'
-import mabiDesktop from '../assets/imgs/gif/mabiDesktop.gif'
+import lContatosDesktop from '../assets/imgs/gif/lContatosDesktop.mp4'
+import lTarefasDesktop from '../assets/imgs/gif/lTarefasDesktop.mp4'
+import orvDesktop from '../assets/imgs/gif/orvDesktop.mp4'
+import educaDesktop from '../assets/imgs/gif/educaDesktop.mp4'
+import mabiDesktop from '../assets/imgs/gif/mabiDesktop.mp4'
 
 
 import lContatos from '../assets/imgs/projetos/lContatos.jpg'
